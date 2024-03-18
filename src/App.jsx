12993964b0f7cc969294281578f7ebed49754fb8
Prity25-coder <pritynishad480@gmail.com>
2024-03-18 19:23:08 +0530

    create keypad button and add calculator style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,126 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
 
   return (
-  <>
-    <h1>Calculator App</h1>
-  </>
-  )
+    <>
+      <h1>React Calculator </h1>
+      <div className="container">
+        <div className="calculator">
+          <form action="">
+            <div className="display">
+              <input type="text" value={value} />
+            </div>
+            <div>
+              <input type="button" value="AC" onClick={(e) => setValue("")} />
+              <input
+                type="button"
+                value="DE"
+                onClick={(e) => setValue(value.slice(0, -1))}
+              />
+              <input
+                type="button"
+                value="."
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="/"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="button"
+                value="7"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="8"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="9"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="*"
+                className="multiple"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="button"
+                value="4"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="5"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="6"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="+"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="button"
+                value="1"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="2"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="3"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="-"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="button"
+                value="00"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="0"
+                onClick={(e) => setValue(value + e.target.value)}
+              />
+              <input
+                type="button"
+                value="="
+                className="equal"
+                onClick={(e) => setValue(eval(value))}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
